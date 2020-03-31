299fd6b756119b02ac1f48f8e9f3ea5c1ff990eb
Isah Ohieku <isahohieku@gmail.com>
2020-03-31 20:58:36 +0100

    updated version and removed unused service export

--- projects/horizontal-scroll-menu/src/public-api.ts
+++ projects/horizontal-scroll-menu/src/public-api.ts
@@ -2,6 +2,5 @@
  * Public API Surface of horizontal-scroll-menu
  */
 
-export * from './lib/horizontal-scroll-menu.service';
 export * from './lib/horizontal-scroll-menu.component';
 export * from './lib/horizontal-scroll-menu.module';

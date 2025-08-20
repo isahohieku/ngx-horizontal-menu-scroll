d5d5de68e807781ae5e0852a0c161817280738bd
Isah Ohieku <isahohieku@gmail.com>
2025-08-20 15:41:04 +0100

    feat: update Karma testing configuration
    - Replace karma-coverage-istanbul-reporter with karma-coverage
    - Update coverage reporter configuration for modern Karma
    - Add coverage to reporters list
    - Configure coverage output directory and report formats

--- projects/horizontal-scroll-menu/karma.conf.js
+++ projects/horizontal-scroll-menu/karma.conf.js
@@ -9,18 +9,22 @@ module.exports = function (config) {
       require('karma-jasmine'),
       require('karma-chrome-launcher'),
       require('karma-jasmine-html-reporter'),
-      require('karma-coverage-istanbul-reporter'),
+      require('karma-coverage'),
       require('@angular-devkit/build-angular/plugins/karma')
     ],
     client: {
       clearContext: false // leave Jasmine Spec Runner output visible in browser
     },
-    coverageIstanbulReporter: {
+    coverageReporter: {
       dir: require('path').join(__dirname, '../../coverage/horizontal-scroll-menu'),
-      reports: ['html', 'lcovonly', 'text-summary'],
-      fixWebpackSourcePaths: true
+      subdir: '.',
+      reporters: [
+        { type: 'html' },
+        { type: 'text-summary' },
+        { type: 'lcovonly' }
+      ]
     },
-    reporters: ['progress', 'kjhtml'],
+    reporters: ['progress', 'kjhtml', 'coverage'],
     port: 9876,
     colors: true,
     logLevel: config.LOG_INFO,
